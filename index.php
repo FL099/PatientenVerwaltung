21f4d18a5caea9e1c16e74a6499769aa03b46cff
Florian <FL099@users.noreply.github.com>
2020-05-23 11:43:17 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+
+?>
+
+<!doctype html>
+<html lang="de">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="res/css/gen.css">
+    <link rel="stylesheet" href="res/css/mystyle.css">
+
+
+    <script src="res/js/app.js"></script>
+
+    <title>PictureCloud</title>
+    <link rel="icon" href="res/img/Cloud.ico" type="image/x-icon">
+    <!--TODO: falls es kein .ico gibt mit .png versuchen--- <link rel="shortcut icon" type="image/png" href="res/img/Cloud.png"/>-->
+</head>
+<body>
+</body>
+</html>
